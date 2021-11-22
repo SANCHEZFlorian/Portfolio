71a80c2466121c83a767f452e0a7901e6fe4260c
Florian SANCHEZ (Ascorio) <ridley17@hotmail.fr>
2021-11-22 02:00:10 +0100

    Début du site, ajout de la page maintenance en attendant le site, et hébergement sur sanchez-florian.com

---
+++ bgGradient.js
@@ -0,0 +1,23 @@
+function generate() {
+    // Fonction de génération du code Hexadécimal
+    var hexValues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e"];
+    function populate(a) {
+        for (var i = 0; i < 6; i++) {
+            var x = Math.round(Math.random() * 14);
+            var y = hexValues[x];
+            a += y;
+        }
+        return a;
+    }
+
+    // Génère les deux codes Hexadécimal et l'angle du gradient
+    var newColor1 = populate('#');
+    var newColor2 = populate('#');
+    var angle = Math.round(Math.random() * 360);
+
+    var gradient = "linear-gradient(" + angle + "deg, " + newColor1 + ", " + newColor2 + ")";
+
+    document.querySelector(".enConstruction").style.background = gradient;
+}
+
+document.addEventListener("DOMContentLoaded", generate());
