dcca8b087cebced638c80a91cfc640e8514f1cae
Florian SANCHEZ (Ascorio) <ridley17@hotmail.fr>
2021-11-23 13:50:22 +0100

    Ajout de la page construction, et construction de celle-ci en cours

--- bgGradient.js
+++ bgGradient.js
@@ -1,6 +1,7 @@
 function generate() {
     // Fonction de génération du code Hexadécimal
     var hexValues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e"];
+
     function populate(a) {
         for (var i = 0; i < 6; i++) {
             var x = Math.round(Math.random() * 14);
@@ -17,7 +18,16 @@ function generate() {
 
     var gradient = "linear-gradient(" + angle + "deg, " + newColor1 + ", " + newColor2 + ")";
 
-    document.querySelector(".enConstruction").style.background = gradient;
+    document.querySelector(".box").style.background = gradient;
+
+
+    // var newColor3 = populate('#');
+    // var newColor4 = populate('#');
+    // var angle2 = Math.round(Math.random() * 360);
+
+    // var gradient2 = "-webkit-linear-gradient(" + newColor3 + ", " + newColor4 + ") ; -webkit-background-clip: text -webkit-text-fill-color: transparent";
+
+    // document.querySelector(".titre").style.background = gradient2;
 }
 
 document.addEventListener("DOMContentLoaded", generate());
